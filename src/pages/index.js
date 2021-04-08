effeb58b41005ce7319fabd3038a6b7287f42b52
abhiaiyer91 <abhiaiyer91@gmail.com>
2021-04-08 09:07:45 -0700

    starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, graphql } from 'gatsby';
-import Layout from '../components/layout';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/layout";
 
-const Index = ({data}) => {
-  const union = data.allKontentItemBlogpostReference.edges.concat(data.allKontentItemProjectReference.edges).concat(data.allKontentItemSpeakingEngagement.edges);
-
-  const items = union.map(({node}) => {
-    if (node.fields && node.fields !== null && node.fields.templateName && node.fields.templateName !== null) {
-      const name = node.fields.templateName === `project-reference` ? node.elements.name___teaser_image__name.value : node.elements.name.value;
-
-      return (
-        <li key={node.id}>
-          <Link to={`../${node.fields.templateName}/${node.fields.slug}`}>
-            {name}
-          </Link>
-        </li>
-      );
-    } else {
-      return (
-        <li key={node.id}>
-          <a href={node.elements.url.value}>
-            {node.elements.name___teaser_image__name.value} (external)
-          </a>
-        </li>
-      );
-    }
-  });
-
+const Index = ({ data }) => {
+  console.log(data);
   return (
     <Layout>
       <div>
-        {items}
+        {data?.allKontentItemBlog?.edges?.map(({ node }) => {
+          return (
+            <div>
+              {" "}
+              <p>{node?.id}</p>
+              <p>{node?.elements?.hello?.value}</p>
+              <img src={node?.elements?.image?.value[0]?.url} />
+            </div>
+          );
+        })}
       </div>
     </Layout>
   );
@@ -41,51 +27,22 @@ export default Index;
 
 export const query = graphql`
   {
-    allKontentItemBlogpostReference(filter: {preferred_language: {eq: "default"}}) {
+    allKontentItemBlog {
       edges {
         node {
           id
           elements {
-            url {
+            hello {
+              name
               value
             }
-            name___teaser_image__name {
-              value
-            }
-          }
-        }
-      }
-    }
-    allKontentItemProjectReference(filter: {preferred_language: {eq: "default"}}) {
-      edges {
-        node {
-          fields {
-            templateName
-            slug
-          }
-          id
-          elements {
-            url {
-              value
-            }
-            name___teaser_image__name {
-              value
-            }
-          }
-        }
-      }
-    }
-    allKontentItemSpeakingEngagement(filter: {preferred_language: {eq: "default"}}) {
-      edges {
-        node {
-          fields {
-            templateName
-            slug
-          }
-          id
-          elements {
-            name {
-              value
+            image {
+              name
+              type
+              value {
+                description
+                url
+              }
             }
           }
         }
