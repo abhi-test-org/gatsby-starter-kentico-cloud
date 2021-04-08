0138398c850f1ee51fb853be2d43d2ed35934916
abhiaiyer91 <abhiaiyer91@gmail.com>
2021-04-08 09:16:22 -0700

    starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.GATSBY_CLOUD !== `true`) {
+  require("dotenv").config();
+}
 
 module.exports = {
   siteMetadata: {
@@ -7,16 +9,22 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: '@kentico/gatsby-source-kontent',
+      resolve: "@kentico/gatsby-source-kontent",
       options: {
         projectId: process.env.KONTENT_PROJECT_ID, // Fill in your Project ID
         // if false used authorization key for secured API
-        usePreviewUrl: process.env.KONTENT_PREVIEW_ENABLED && process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true',
-        authorizationKey: process.env.KONTENT_PREVIEW_ENABLED && process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true'
-          ? process.env.KONTENT_PREVIEW_KEY
-          : undefined,
-        languageCodenames: process.env.KONTENT_LANGUAGE_CODENAMES.split(',').map(lang => lang.trim()),
+        usePreviewUrl:
+          process.env.KONTENT_PREVIEW_ENABLED &&
+          process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === "true",
+        authorizationKey:
+          process.env.KONTENT_PREVIEW_ENABLED &&
+          process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === "true"
+            ? process.env.KONTENT_PREVIEW_KEY
+            : undefined,
+        languageCodenames: process.env.KONTENT_LANGUAGE_CODENAMES.split(
+          ","
+        ).map((lang) => lang.trim()),
       },
     },
-  ]
+  ],
 };
